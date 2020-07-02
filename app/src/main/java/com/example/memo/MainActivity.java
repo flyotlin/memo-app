7b6aa649c525a695a0634a5f5075cf9c1145e74b
flyotlin <flyotlin@gmail.com>
2020-07-03 00:19:19 +0800

    Add new/delete memo function

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -16,10 +16,10 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 // @auther: flyotlin on 2020.07.01
 
-// More function on each memo
 // Database
 // ItemTouchHelper
 public class MainActivity extends AppCompatActivity {
@@ -28,10 +28,10 @@ public class MainActivity extends AppCompatActivity {
     private Button addMemo;
 
     private RecyclerView memoRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private MyAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
-    private String[] dataset = {"ABCDDD", "2216sdf", "fdf1", "2216sdf", "fdf1", "2216sdf", "fdf1"};
+    private ArrayList<String> dataset = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,22 +53,10 @@ public class MainActivity extends AppCompatActivity {
         addMemo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateDataset();
-                updateRecyclerAdapter();
+                mAdapter.addMemo(toDo.getText().toString());
+                toDo.setText("");
             }
         });
-    }
-
-    private void updateDataset() {
-        int len = dataset.length;
-        String[] tmp = new String[len+1];
-        for(int i = 1; i <= len; i++) {
-            tmp[i] = dataset[i-1];
-        }
-        tmp[0] = toDo.getText().toString();
-        dataset = tmp;
-
-        toDo.setText("");
     }
 
     private void setRecyclerView() {
@@ -77,22 +65,9 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         memoRecyclerView.setLayoutManager(layoutManager);
 
-
-
-        mAdapter = new MyAdapter(dataset);
-
-
-        memoRecyclerView.setAdapter(mAdapter);
-
-
-    }
-
-    private void updateRecyclerAdapter() {
         mAdapter = new MyAdapter(dataset);
         memoRecyclerView.setAdapter(mAdapter);
     }
-
-
 
 
 
--- app/src/main/java/com/example/memo/MyAdapter.java
+++ app/src/main/java/com/example/memo/MyAdapter.java
@@ -2,49 +2,70 @@ package com.example.memo;
 
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private String[] mDataset;
+    private ArrayList<String> mDataset;
 
-    public MyAdapter(String[] myDataset) {
+    public MyAdapter(ArrayList<String> myDataset) {
         this.mDataset = myDataset;
     }
 
     @NonNull
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // inflate
-        TextView textView = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.text_view, parent, false);
-
-        ViewHolder holder = new ViewHolder(textView);
+        LinearLayout memoLayout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_memo, parent, false);
+        ViewHolder holder = new ViewHolder(memoLayout);
 
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
-        holder.textView.setText(mDataset[position]);
-
+        holder.textView.setText(mDataset.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset.size();
     }
 
+    public void addMemo(String str) {
+        mDataset.add(0, str);
+        notifyDataSetChanged();
+    }
+
+    public void removeMemo(int position) {
+        mDataset.remove(position);
+        notifyDataSetChanged();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
-        public ViewHolder(@NonNull TextView v) {
-            super(v);
-            textView = v;
+        public Button deleteButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.memo_view);
+            deleteButton = (Button) itemView.findViewById(R.id.deleteButton);
+
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    removeMemo(getAdapterPosition());
+                }
+            });
         }
     }
 }
